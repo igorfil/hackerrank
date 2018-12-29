2cfaed91bf2b8f54a61f6a7540aae7967cc88d78
igor <igor.v.fil@gmail.com>
2018-12-28 19:19:32 -0800

    Minimum difference

---
+++ java/src/Arrays/MinimumAbsoluteDifference.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinimumAbsoluteDifference {
+    static int minimumAbsoluteDifference(int[] arr) {
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length - 1; ++i) {
+            int diff = Math.abs(arr[i] - arr[i + 1]);
+            if (diff == 0) {
+                return diff;
+            }
+
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+
+        return minDiff;
+    }
+
+    @Test
+    void test() {
+        assertEquals(0, minimumAbsoluteDifference(new int[]{1, 1}));
+        assertEquals(1, minimumAbsoluteDifference(new int[]{1, 2, 10}));
+        assertEquals(0, minimumAbsoluteDifference(new int[]{1, 20, 100, 100, 500}));
+    }
+}
